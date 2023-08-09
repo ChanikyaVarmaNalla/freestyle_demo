39fadb7047da51a8a20a9c81f3e7252c9588f920
ChanikyaVarmaNalla <141623689+ChanikyaVarmaNalla@users.noreply.github.com>
2023-08-09 14:03:25 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,7 +1,6 @@
 # Factorial of a number
 n = 5
 fact = 1
-for i in range(n, 1, -1):
+for i in range(n, 0, -1):
     fact = fact * i
-print(f"Factorial of {n} is {fact}")
 print(f"Factorial of {n} is {fact}")
